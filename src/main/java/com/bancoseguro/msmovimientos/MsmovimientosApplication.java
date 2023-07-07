c10706bbe95b5d593cbbd6d574cf13a45fa11555
Luis Delgado <37703993+delgadol@users.noreply.github.com>
2023-07-07 12:37:07 -0500

    Inicializacion
    Inicializacion

---
+++ src/main/java/com/bancoseguro/msmovimientos/MsmovimientosApplication.java
@@ -0,0 +1,13 @@
+package com.bancoseguro.msmovimientos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsmovimientosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsmovimientosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bancoseguro/msmovimientos/MsmovimientosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bancoseguro.msmovimientos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsmovimientosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
